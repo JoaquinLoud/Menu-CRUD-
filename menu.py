e2ec922f64983ae1c0a4a9826b54960d51798bbf
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-09-01 14:46:41 -0500

    creamos la condicion cuando es =1

--- menu.py
+++ menu.py
@@ -10,3 +10,14 @@ def opciones():
      print("4............ELIMINAR")
      print("5............SALIR")
     opcion = int(input("Digite una opcion: "))
+    
+    if (opcion == 1):
+            tamañao = len(diccionario)
+            try:
+                funcion = input("Digite el nombre de la funcion: ")
+                descripcionFun = input("Digite la descripcion de la funcion: ")
+                diccionario[tamañao + 1] = (descripcionFun, funcion)
+            except:
+                print('Error dato invalido')
+            print(diccionario)
+  
