fe9b0f1112b290266064e09271f4e2788f5d4cf3
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-09-01 14:45:53 -0500

    variable para dijitar opcion

--- menu.py
+++ menu.py
@@ -3,9 +3,10 @@ def opciones():
     salir = False
     while (salir == False):
      tamañao = len(diccionario)
-            print("Bienvenido Al Menu\n\n")
-            print("1............AGREGAR")
-            print("2............MOSTRAR")
-            print("3............EDITAR")
-            print("4............ELIMINAR")
-            print("5............SALIR")
+     print("Bienvenido Al Menu\n\n")
+     print("1............AGREGAR")
+     print("2............MOSTRAR")
+     print("3............EDITAR")
+     print("4............ELIMINAR")
+     print("5............SALIR")
+    opcion = int(input("Digite una opcion: "))
