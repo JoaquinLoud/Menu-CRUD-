6e267b9928707db328b587f34e0120b0b3c38933
Francia Tile Uriana <111076189+Ftile@users.noreply.github.com>
2022-09-01 20:07:42 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -12,53 +12,8 @@ def opciones():
     opcion = int(input("Digite una opcion: "))
     
     if (opcion ==1):
+        tamañao = len(diccionario)
         
-             tamañao = len(diccionario)
-            try:
-                funcion = input("Digite el nombre de la funcion: ")
-                descripcionFun = input("Digite la descripcion de la Funcion: ")
-                diccionario[tamañao + 1] = (descripcionFun, funcion)
-            except:
-                print('Error dato invalido')
-            print(diccionario)
-    if (opcion == 2):
-            if(tamañao > 0):
-                print("Bienvenido Al Listado de Funciones\n\n")
-                for x in diccionario:
-                    print(f"{x}. {diccionario[x][0]}")
-                salida = False
-                while (salida == False):
-                    try:
-                        op = int(input("Digite una opcion para Eliminar: "))
-                        salida = True
-                    except:
-                        print('Error dato invalido')
-                for clave in diccionario:
-                    if (op == clave):
-                        print("Esta funcion se llama: ", diccionario[clave][1])
-            else: 
-                print('\n\n" No se han agregado funciones...')
-
-    if (opcion == 3):
-            if(tamañao > 0):
-                print("Bienvenido Al Listado de Funciones\n\n")
-                for x in diccionario:
-                    print(f"{x}. {diccionario[x][0]}")
-                salida = False
-                while (salida == False):
-                    try:
-                        op = int(input("Digite una opcion para Eliminar: "))
-                        salida = True
-                    except:
-                        print('Error dato invalido')
-                for clave in diccionario:
-                    if (op == clave):
-                        funcion = input("Digite el nuevo nombre de la Funcion: ")
-                        descripcionFun = input(
-                            "Digite la nueva descripcion de la funcion: ")
-                        diccionario[clave] = (descripcionFun, funcion)
-            else: 
-                print('\n\n" No se han agregado funciones...')
     if (opcion == 4):
             if(tamañao > 0):
                 print("Bienvenido Al Listado de funciones\n\n")
